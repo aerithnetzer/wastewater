c80ef1de672c5ac4436828f82d9c19109e229131
aerithnetzer <aerithnetzer@outlook.com>
2025-03-14 10:33:21 -0500

    Change hours needed in job to 1

--- submission_script.sh
+++ submission_script.sh
@@ -4,8 +4,8 @@
 #SBATCH --gres=gpu:100:1
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH -t 08:00:00
-#SBATCH --mem=64G
+#SBATCH -t 01:00:00
+#SBATCH --mem=32G
 #SBATCH --job-name=wastewater_analysis
 #SBATCH --output=%j.out
 #SBATCH --error=%j.err
